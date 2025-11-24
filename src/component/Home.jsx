c588216ff40bdab28459240829d61b43bd5217c1
Shashikant Yadav <yshashikant231217@gmail.com>
2025-11-24 14:38:42 +0530

    Remove unnecessary whitespace and comments in Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -44,7 +44,6 @@ function Home() {
         },
       ],
     };
-
     try {
       const res = await fetch(API_URL, {
         method: "POST",
@@ -94,7 +93,6 @@ function Home() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 h-screen">
-  {/* Sidebar */}
   <div className="hidden md:block md:col-span-1">
     <Sidebar
       recentHistory={local}
