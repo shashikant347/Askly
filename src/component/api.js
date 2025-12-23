7644151cee9f1f6319bc94cfdb1914c42bd6ee76
Shashikant Yadav <yshashikant231217@gmail.com>
2025-12-23 14:40:29 +0530

    Update api.js

--- src/component/api.js
+++ src/component/api.js
@@ -1,4 +1,4 @@
 // api.js
 const API_KEY = "AIzaSyCHRjiGc0qatrHT3upNOGqliJgqD5f-IQ8"; 
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`;
 export default API_URL;
