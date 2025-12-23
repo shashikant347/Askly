f3d20d487e452ce0dc3b2786d4c370c40b5b1b42
Shashikant Yadav <yshashikant231217@gmail.com>
2025-12-23 17:00:31 +0530

    Update api.js

--- src/component/api.js
+++ src/component/api.js
@@ -1,4 +1,6 @@
-// api.js
+]// api.js
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/text-bison-001:generateText?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 export default API_URL;
+
+
